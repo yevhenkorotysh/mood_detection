357c8470801d7810c7b7e8425b1fdc87d9f8d53f
yevhenkorotysh <korotysh@gmail.com>
2023-10-30 00:25:19 -0700

    requirements

--- mood_detection/settings.py
+++ mood_detection/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-4!%nqudmcies@a=9@pny-68p$hwaf_i5k7pf=+h#1mxsu)ue9*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "django-env.eba-9xpeamqy.us-west-2.elasticbeanstalk.com",
+    "127.0.0.1",
+
+]
 
 
 # Application definition
@@ -129,8 +133,12 @@ USE_TZ = True
 
 APPEND_SLASH = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
