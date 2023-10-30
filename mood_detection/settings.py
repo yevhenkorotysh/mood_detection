d6760661f79c7c33f4e95fc8015bd4420bce9361
yevhenkorotysh <korotysh@gmail.com>
2023-10-30 01:36:48 -0700

    debug=false

--- mood_detection/settings.py
+++ mood_detection/settings.py
@@ -139,7 +139,7 @@ APPEND_SLASH = True
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static_files/')
 # ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static_files')
 STATIC_URL = '/static/'
 
 print(STATIC_ROOT)
@@ -147,6 +147,7 @@ print(STATIC_ROOT)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
